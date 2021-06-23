fd9611ce68dc9e6cb816a99a3331c1108b2feddc
Tatum Mortimer <tatumdmortimer@gmail.com>
2021-06-23 09:25:21 -0600

    updated memory limit and added latency wait tim

--- start_snakemake.sh
+++ start_snakemake.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1000
+#SBATCH --mem=2000
 #SBATCH -p cpu2019
 #SBATCH -t 3-00:00
 #SBATCH -o snakemake.out
@@ -11,4 +11,4 @@
 #SBATCH --mail-user=
 
 mkdir -p logs/slurm
-snakemake --profile slurm
+snakemake --profile slurm --rerun-incomplete --latency-wait 90
